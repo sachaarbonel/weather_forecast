18d9ffc41fbc5ef16d62e9776d25eb032deb1646
Sacha Arbonel <sacha.arbonel@hotmail.fr>
2021-01-03 23:44:48 -0400

    checkScope test

--- lib/utils/date.dart
+++ lib/utils/date.dart
@@ -1,4 +1,5 @@
 import 'package:intl/intl.dart';
+import 'package:quiver/src/time/clock.dart';
 
 String formatHourMinute(int timestamp) {
   final hourMinuteFormat = DateFormat(DateFormat.HOUR_MINUTE);
@@ -9,4 +10,11 @@ String formatHourMinute(int timestamp) {
 String formatWeekDay(int timestamp) {
   final weekDayFormat = DateFormat(DateFormat.WEEKDAY);
   return weekDayFormat.format(DateTime.fromMillisecondsSinceEpoch(timestamp));
+}
+
+bool checkScope(DateTime other, [Clock clock]) {
+  final now = clock?.now() ?? DateTime.now();
+  final isInScope = other.difference(now).inDays < Duration(days: 8).inDays;
+  print(now.difference(other));
+  return isInScope;
 }
--- test/utils_test.dart
+++ test/utils_test.dart
@@ -1,3 +1,4 @@
+import 'package:quiver/testing/src/async/fake_async.dart';
 import 'package:test/test.dart';
 import 'package:weather_forecast/utils/date.dart';
 
@@ -8,5 +9,14 @@ void main() {
 
   test('test date utils formatWeekDay', () {
     expect(formatWeekDay(1610100000), 'Monday');
+  });
+
+  test('checkScope test', () {
+    FakeAsync().run((self) {
+      final clock = self.getClock(DateTime(2021, 01, 03));
+
+      expect(checkScope(DateTime(2021, 01, 05), clock), true);
+      expect(checkScope(DateTime(2021, 01, 11), clock), false);
+    });
   });
 }
